f05a3669912b26ec9d60c73238af7e1e546d27d3
Kruck <75646745+ThorrenKruck@users.noreply.github.com>
2023-09-21 14:23:35 +0200

    Create hping.ps1

---
+++ hping.ps1
@@ -0,0 +1,77 @@
+### hping.ps1
+### Powered by Krucktech
+### V1.0 - 21.09.2023
+###
+### Advanced ping by hostname. If the target was initially reachable, wait until ping fails and succeeds again. If the target was initially not reachable, wait until ping succeeds. Powershell7-proof. Intended to be called in a shell with the target as the argument.
+
+param ([string] $target)
+$repeat = $true
+$answer = "y"
+
+$noConnection = $true
+$counterNoConnection= 1
+$counterStillConnected = 1
+$initiallyConnected = $false
+
+$running=$true
+while ($running)
+{
+    if(Test-Connection -ComputerName $target -Count 1 -Quiet)
+    {
+        $ping = Test-Connection -ComputerName $target -Count 1
+        $time = (Get-Date | Select-Object TimeOfDay).TimeofDay
+        if($PSVersionTable.PSVersion.Major -eq 7)
+        {
+            Write-Host "still connected to $target with ip $($ping.Address.IPAddressToString) #$counterStillConnected at $time" -ForegroundColor Yellow
+        }
+        else
+        {
+            Write-Host "still connected to $target with ip $($ping.IPV4Address.IPAddressToString) #$counterStillConnected at $time" -ForegroundColor Yellow
+        }        
+        $counterStillConnected += 1
+        $initiallyConnected = $true
+        Start-Sleep -Seconds 1
+    } 
+    else 
+    {
+        while ($noConnection)
+        {
+            if(Test-Connection -ComputerName $target -Count 1 -Quiet)
+            {
+                $ping = Test-Connection -ComputerName $target -Count 1
+                $time = (Get-Date | Select-Object TimeOfDay).TimeofDay
+                if ($initiallyConnected = $true)
+                {
+                    if($PSVersionTable.PSVersion.Major -eq 7)
+                    {
+                        Write-Host "COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONNECTED again to $target with ip $($ping.Address.IPAddressToString) at $time" -ForegroundColor Green
+                    }
+                    else
+                    {
+                        Write-Host "COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONNECTED again to $target with ip $($ping.IPV4Address.IPAddressToString) at $time" -ForegroundColor Green
+                    }  
+                }
+                else
+                {
+                    if($PSVersionTable.PSVersion.Major -eq 7)
+                    {
+                        Write-Host "COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONNECTED initially to $target with ip $($ping.Address.IPAddressToString) at $time" -ForegroundColor Green
+                    }
+                    else
+                    {
+                        Write-Host "COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONNECTED initially to $target with ip $($ping.IPV4Address.IPAddressToString) at $time" -ForegroundColor Green
+                    }  
+                }
+                $noConnection = $false
+                $running = $false
+            } 
+            else 
+            {
+                $time = (Get-Date | Select-Object TimeOfDay).TimeofDay
+                Write-Host "no connection to $target #$counterNoConnection at $time" -ForegroundColor Red
+                $counterNoConnection+= 1
+                Start-Sleep -Seconds 1
+            }
+        }
+    }
+}
