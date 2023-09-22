67fb502211a2475b37a0ea73004c8af8d37e3f28
Kruck <75646745+ThorrenKruck@users.noreply.github.com>
2023-09-22 09:25:42 +0200

    Create get-lastlogontimestampcomputer.ps1

---
+++ get-lastlogontimestampcomputer.ps1
@@ -0,0 +1,5 @@
+### get-lastlogontimestampcomputer
+### Powered by Krucktech
+### V1.0 - 22.09.2023
+###
+### Gets the last time a computer has logged on to AD. !! The value is only replicated every 9 to 14 days by default and thus may not be accurate to the day !!
