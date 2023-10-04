791a991b9bb81b05ef81dc722c9803791ebc39f5
Kruck <75646745+ThorrenKruck@users.noreply.github.com>
2023-10-04 08:30:56 +0200

    Update Get-EmptyADGroups.ps1
    Added error handling for when ad group members can not be fetched.

--- Get-EmptyADGroups.ps1
+++ Get-EmptyADGroups.ps1
@@ -1,19 +1,30 @@
 ### Get-EmptyADGroups.ps1
 ### Powered by Krucktech
-### V1.0 - 22.09.2023
+### V1.1 - 04.10.2023
 ###
 ### Gets all AD groups without any members.
 
 $count0 = 0
 $count1 = 0
 $countMany = 0
+$countError = 0
 
 Write-Host "fetching groups..."
 $allgroups = Get-ADGroup -Filter * | sort name
 Write-Host "checking groups for members..."
 foreach($group in $allgroups)
 {
-    $members = Get-ADGroupMember $group
+    $members = $null
+    try
+    {
+        $members = Get-ADGroupMember $group
+    }
+    catch
+    {
+        Write-Host "Error with group $($group.Name)" -ForegroundColor Magenta
+        $countError++
+        continue
+    }
     if($members -eq $null)
     {
         Write-Host "$($group.Name) has no members" -ForegroundColor Red
@@ -29,12 +40,13 @@ foreach($group in $allgroups)
         else
         {
             Write-Host "$($group.Name) has 1 member" -ForegroundColor Yellow
-            $count1++
+            $count1++ 
         }
     }
 }
 Write-Host ""
-Write-Host "Total Groups in AD:        $($allGroups.count)"
-Write-Host "Groups with 0 members:     $count0" -ForegroundColor Red
-Write-Host "Groups with 1 member:      $count1" -ForegroundColor Yellow
-Write-Host "Groups more than 1 member: $countMany" -ForegroundColor Green
+Write-Host "Total Groups in AD:             $($allGroups.count)"
+Write-Host "Groups with 0 members:          $count0" -ForegroundColor Red
+Write-Host "Groups with 1 member:           $count1" -ForegroundColor Yellow
+Write-Host "Groups with more than 1 member: $countMany" -ForegroundColor Green
+Write-Host "Groups with error:              $countError" -ForegroundColor Magenta
