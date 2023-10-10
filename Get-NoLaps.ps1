e8693f3c98a9d9c0531c58bf294486bf96ed809d
Kruck <75646745+ThorrenKruck@users.noreply.github.com>
2023-10-10 13:49:32 +0200

    Update Get-NoLaps.ps1
    Removed filtering by last logon and added the value to output instead.

--- Get-NoLaps.ps1
+++ Get-NoLaps.ps1
@@ -1,13 +1,9 @@
 ### get-nolaps
 ### Powered by Krucktech
-### V1.1 - 25.09.2023
+### V1.2 - 10.10.2023
 ###
-### Get all enabled computer objects with compatible OS from AD that have contacted the AD in a specified amount of days and do not have an encrypted LAPS password set.
+### Get all enabled computer objects with compatible OS from AD that do not have an encrypted LAPS password set.
 
-# set timespan in days in which the systems need to have contacted AD
-$timespan = 180
-
-$date = (date).AddDays(-$timespan)
-$nolaps = Get-ADComputer -Filter {Enabled -eq "True" -and lastlogon -ge $date -and (OperatingSystem -like "Windows Server 2019*" -or OperatingSystem -like "Windows Server 2022*" -or OperatingSystem -like "Windows 1*")} -Properties mslaps-encryptedpassword,lastlogon,operatingsystem | where {$_.'mslaps-encryptedpassword' -eq $null} | select name,operatingsystem,mslaps-encryptedpassword | sort operatingsystem,name
+$nolaps = Get-ADComputer -Filter {Enabled -eq "True" -and (OperatingSystem -like "Windows Server 2019*" -or OperatingSystem -like "Windows Server 2022*" -or OperatingSystem -like "Windows 1*")} -Properties mslaps-encryptedpassword,operatingsystem,lastlogondate | where {$_.'mslaps-encryptedpassword' -eq $null} | select name,operatingsystem,lastlogondate,mslaps-encryptedpassword | sort operatingsystem,name
 Write-Host "Systems without LAPS: $($nolaps.count)"
-$nolaps
+$nolaps | ft -a
