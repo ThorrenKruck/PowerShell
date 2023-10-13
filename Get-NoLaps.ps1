288d85538cdd98c44f59952790b2b0de022cb7b0
Kruck <75646745+ThorrenKruck@users.noreply.github.com>
2023-10-13 10:56:25 +0200

    Update Get-NoLaps.ps1
    Added filter to exclude domain controllers.

--- Get-NoLaps.ps1
+++ Get-NoLaps.ps1
@@ -1,9 +1,10 @@
 ### get-nolaps
 ### Powered by Krucktech
-### V1.2 - 10.10.2023
+### V1.3 - 10.10.2023
 ###
-### Get all enabled computer objects with compatible OS from AD that do not have an encrypted LAPS password set.
+### Get all enabled computer objects with compatible OS, except DCs, from AD that do not have an encrypted LAPS password set.
 
-$nolaps = Get-ADComputer -Filter {Enabled -eq "True" -and (OperatingSystem -like "Windows Server 2019*" -or OperatingSystem -like "Windows Server 2022*" -or OperatingSystem -like "Windows 1*")} -Properties mslaps-encryptedpassword,operatingsystem,lastlogondate | where {$_.'mslaps-encryptedpassword' -eq $null} | select name,operatingsystem,lastlogondate,mslaps-encryptedpassword | sort operatingsystem,name
+$dcs = (Get-ADDomainController -Filter *).name
+$nolaps = Get-ADComputer -Filter {Enabled -eq "True" -and (OperatingSystem -like "Windows Server 2019*" -or OperatingSystem -like "Windows Server 2022*" -or OperatingSystem -like "Windows 1*")} -Properties mslaps-encryptedpassword,operatingsystem,lastlogondate | where {$_.'mslaps-encryptedpassword' -eq $null -and $dcs -notcontains $_.Name} | select name,operatingsystem,lastlogondate,mslaps-encryptedpassword | sort operatingsystem,name
 Write-Host "Systems without LAPS: $($nolaps.count)"
 $nolaps | ft -a
