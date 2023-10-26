68bcca253d7a4da055512ce7ee8fb6b52b7c6bca
Kruck <75646745+ThorrenKruck@users.noreply.github.com>
2023-10-26 11:41:42 +0200

    Update Get-LastLogonTimestampComputer.ps1
    The entire code was missing, wtf

--- Get-LastLogonTimestampComputer.ps1
+++ Get-LastLogonTimestampComputer.ps1
@@ -3,3 +3,10 @@
 ### V1.0 - 22.09.2023
 ###
 ### Gets the last time a computer has logged on to AD. !! The value is only replicated every 9 to 14 days by default and thus may not be accurate to the day !!
+
+while($true)
+{
+    $computer = Read-Host -Prompt "Computername"
+    Get-ADComputer $computer -Properties LastLogonDate | select LastLogonDate
+    Write-Host "------------------------------"
+}
